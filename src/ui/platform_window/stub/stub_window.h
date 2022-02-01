jdapena/chromium84

[op][agl][wayland][agl_shell] Add agl-shell protocol support.
Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

src/ui/platform_window/stub/stub_window.h
@@ -36,6 +36,7 @@ class STUB_WINDOW_EXPORT StubWindow : public PlatformWindow {
   void SetBounds(const gfx::Rect& bounds) override;
   gfx::Rect GetBounds() override;
   void SetTitle(const base::string16& title) override;
+  void SetAppId(const base::string16& title) override;
   void SetCapture() override;
   void ReleaseCapture() override;
   void ToggleFullscreen() override;
@@ -57,7 +58,10 @@ class STUB_WINDOW_EXPORT StubWindow : public PlatformWindow {
                       const gfx::ImageSkia& app_icon) override;
   void SizeConstraintsChanged() override;
   void SetSurfaceId(int surface_id) override;
-
+  void SetAglBackground() override;
+  void SetAglPanel(int edge) override;
+  void SetAglReady() override;
+  void SetAglActivateApp(std::string app) override;
   PlatformWindowDelegate* delegate_;
   gfx::Rect bounds_;
 

src/ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -717,6 +717,12 @@ bool DesktopNativeWidgetAura::SetWindowTitle(const base::string16& title) {
   return desktop_window_tree_host_->SetWindowTitle(title);
 }
 
+bool DesktopNativeWidgetAura::SetAglAppId(const base::string16& title) {
+  if (!content_window_)
+    return false;
+  return desktop_window_tree_host_->SetAglAppId(title);
+}
+
 void DesktopNativeWidgetAura::SetWindowIcons(const gfx::ImageSkia& window_icon,
                                              const gfx::ImageSkia& app_icon) {
   if (content_window_)

src/ui/views/widget/desktop_aura/desktop_native_widget_aura.h
@@ -141,6 +141,7 @@ class VIEWS_EXPORT DesktopNativeWidgetAura
   void GetWindowPlacement(gfx::Rect* bounds,
                           ui::WindowShowState* maximized) const override;
   bool SetWindowTitle(const base::string16& title) override;
+  bool SetAglAppId(const base::string16& title) override;
   void SetWindowIcons(const gfx::ImageSkia& window_icon,
                       const gfx::ImageSkia& app_icon) override;
   void InitModalType(ui::ModalType modal_type) override;

src/ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -530,11 +530,53 @@ bool DesktopWindowTreeHostPlatform::SetWindowTitle(
   return true;
 }
 
+bool DesktopWindowTreeHostPlatform::SetAglAppId(const base::string16& title)
+{
+  if (platform_window())
+    platform_window()->SetAppId(title);
+
+  return true;
+}
+
 void DesktopWindowTreeHostPlatform::SetWindowSurfaceId(int surface_id) {
   if (platform_window())
     platform_window()->SetSurfaceId(surface_id);
   else
     pending_surface_id_ = surface_id;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglPanel(int edge)
+{
+  if (platform_window())
+    platform_window()->SetAglPanel(edge);
+  else
+    pending_agl_panel_edge_ = edge;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglBackground(void)
+{
+  if (platform_window())
+    platform_window()->SetAglBackground();
+  else
+    pending_agl_background_ = true;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglReady(void)
+{
+  if (platform_window())
+    platform_window()->SetAglReady();
+  else
+    pending_agl_ready_ = true;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglActivateApp(std::string app)
+{
+	if (platform_window())
+		platform_window()->SetAglActivateApp(app);
 }
 
 void DesktopWindowTreeHostPlatform::ClearNativeFocus() {

src/ui/views/widget/native_widget_aura.cc
@@ -434,6 +434,11 @@ bool NativeWidgetAura::SetWindowTitle(const base::string16& title) {
   return true;
 }
 
+bool NativeWidgetAura::SetAglAppId(const base::string16& title) {
+  NOTIMPLEMENTED_LOG_ONCE();
+  return false;
+}
+
 void NativeWidgetAura::SetWindowIcons(const gfx::ImageSkia& window_icon,
                                       const gfx::ImageSkia& app_icon) {
   AssignIconToAuraWindow(window_, window_icon, app_icon);

src/ui/views/widget/widget.cc
@@ -872,6 +872,13 @@ void Widget::UpdateWindowTitle() {
   non_client_view_->UpdateWindowTitle();
 }
 
+void
+Widget::SetAglAppId(const base::string16 &app_id)
+{
+	native_widget_->SetAglAppId(app_id);
+}
+
+
 void Widget::UpdateWindowIcon() {
   if (non_client_view_)
     non_client_view_->UpdateWindowIcon();

Igalia/chromium87

src/ui/platform_window/stub/stub_window.h
@@ -36,6 +36,7 @@ class STUB_WINDOW_EXPORT StubWindow : public PlatformWindow {
   void SetBounds(const gfx::Rect& bounds) override;
   gfx::Rect GetBounds() override;
   void SetTitle(const base::string16& title) override;
+  void SetAppId(const std::string& title) override;
   void SetCapture() override;
   void ReleaseCapture() override;
   void ToggleFullscreen() override;
@@ -57,7 +58,10 @@ class STUB_WINDOW_EXPORT StubWindow : public PlatformWindow {
                       const gfx::ImageSkia& app_icon) override;
   void SizeConstraintsChanged() override;
   void SetSurfaceId(int surface_id) override;
-
+  void SetAglBackground() override;
+  void SetAglPanel(int edge) override;
+  void SetAglReady() override;
+  void SetAglActivateApp(std::string app) override;
   PlatformWindowDelegate* delegate_;
   gfx::Rect bounds_;
 

src/ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -723,6 +723,12 @@ bool DesktopNativeWidgetAura::SetWindowTitle(const base::string16& title) {
   return desktop_window_tree_host_->SetWindowTitle(title);
 }
 
+bool DesktopNativeWidgetAura::SetAglAppId(const std::string& title) {
+  if (!content_window_)
+    return false;
+  return desktop_window_tree_host_->SetAglAppId(title);
+}
+
 void DesktopNativeWidgetAura::SetWindowIcons(const gfx::ImageSkia& window_icon,
                                              const gfx::ImageSkia& app_icon) {
   if (content_window_)

src/ui/views/widget/desktop_aura/desktop_native_widget_aura.h
@@ -144,6 +144,7 @@ class VIEWS_EXPORT DesktopNativeWidgetAura
   void GetWindowPlacement(gfx::Rect* bounds,
                           ui::WindowShowState* maximized) const override;
   bool SetWindowTitle(const base::string16& title) override;
+  bool SetAglAppId(const std::string& title) override;
   void SetWindowIcons(const gfx::ImageSkia& window_icon,
                       const gfx::ImageSkia& app_icon) override;
   void InitModalType(ui::ModalType modal_type) override;

src/ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -534,11 +534,53 @@ bool DesktopWindowTreeHostPlatform::SetWindowTitle(
   return true;
 }
 
+bool DesktopWindowTreeHostPlatform::SetAglAppId(const std::string& title)
+{
+  if (platform_window())
+    platform_window()->SetAppId(title);
+
+  return true;
+}
+
 void DesktopWindowTreeHostPlatform::SetWindowSurfaceId(int surface_id) {
   if (platform_window())
     platform_window()->SetSurfaceId(surface_id);
   else
     pending_surface_id_ = surface_id;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglPanel(int edge)
+{
+  if (platform_window())
+    platform_window()->SetAglPanel(edge);
+  else
+    pending_agl_panel_edge_ = edge;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglBackground(void)
+{
+  if (platform_window())
+    platform_window()->SetAglBackground();
+  else
+    pending_agl_background_ = true;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglReady(void)
+{
+  if (platform_window())
+    platform_window()->SetAglReady();
+  else
+    pending_agl_ready_ = true;
+}
+
+void
+DesktopWindowTreeHostPlatform::SetAglActivateApp(std::string app)
+{
+	if (platform_window())
+		platform_window()->SetAglActivateApp(app);
 }
 
 void DesktopWindowTreeHostPlatform::ClearNativeFocus() {

src/ui/views/widget/native_widget_aura.cc
@@ -436,6 +436,11 @@ bool NativeWidgetAura::SetWindowTitle(const base::string16& title) {
   return true;
 }
 
+bool NativeWidgetAura::SetAglAppId(const std::string& title) {
+  NOTIMPLEMENTED_LOG_ONCE();
+  return false;
+}
+
 void NativeWidgetAura::SetWindowIcons(const gfx::ImageSkia& window_icon,
                                       const gfx::ImageSkia& app_icon) {
   AssignIconToAuraWindow(window_, window_icon, app_icon);

src/ui/views/widget/widget.cc
@@ -875,6 +875,13 @@ void Widget::UpdateWindowTitle() {
   non_client_view_->UpdateWindowTitle();
 }
 
+void
+Widget::SetAglAppId(const std::string& app_id)
+{
+	native_widget_->SetAglAppId(app_id);
+}
+
+
 void Widget::UpdateWindowIcon() {
   if (non_client_view_)
     non_client_view_->UpdateWindowIcon();